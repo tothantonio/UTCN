ba21223cd17b7e5ec313e39a25b1d9bacfa286f6
Antonio <tothantonio000@gmail.com>
2024-12-10 16:45:03 +0200

    oop

---
+++ Second_Year/OOP/Suplimentar/test2_v2/src/A.java
@@ -0,0 +1,18 @@
+public class A extends B{
+    int valare;
+    static{
+        info += getInfo();
+    }
+    public A() {
+        nume += "Craciun";
+        valoare += 25;
+    }
+
+    public String toString() {
+        return "Cadouri";
+    }
+
+    public static String getInfo() {
+        return "6 ";
+    }
+}
